95d8944734dd7652845cfdfaf29944676f78d751
Kendall Werts <kmwerts@gmail.com>
2019-09-14 15:58:54 -0500

    creates a pdf on submit

--- js/global.js
+++ js/global.js
@@ -68,3 +68,37 @@
     
 
 })(jQuery);
+
+
+$('.download-pdf').click(function () {
+    // Don't forget, that there are CORS-Restrictions. So if you want to run it without a Server in your Browser you need to transform the image to a dataURL
+// Use http://dataurl.net/#dataurlmaker
+    var doc = new jsPDF();
+    doc.text('Hello world!', 20, 20);
+    doc.text('This is client-side Javascript, pumping out a PDF.', 20, 30);
+    doc.addPage('a6', 'l');
+    doc.text('Do you like that?', 20, 20);
+    doc.save('test.pdf');
+    // savePDF(doc);
+});
+
+// function savePDF (doc) {
+//     // $('.download-pdf').click(function () {
+//         // eval('try{' + editor.getValue() + '} catch(e) { console.error(e.message,e.stack,e); }');
+
+//         var file = demos[$('#template').val()];
+//         // if (file === undefined) {
+//         //     file = 'demo';
+//         // }
+//         if (typeof doc !== 'undefined') {
+//             doc.save(file + '.pdf');
+//         } else if (typeof pdf !== 'undefined') {
+//             setTimeout(function () {
+//                 pdf.save(file + '.pdf');
+//             }, 2000);
+//         } else {
+//             alert('Error 0xE001BADF');
+//         }
+//     // });
+//     // return false;
+// }
